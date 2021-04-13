d0d149e1915d73adab687693c6d6324b6f0e5233
SFernando <sandeepa.fernando@texus.lk>
2021-04-13 19:51:45 +0530

    adding onChange event and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 import todosData from "./data/todosData";
 import TodoItem from "./Components/TodoItem";
 
-function App() {
+const App: FC = () => {
+
+  const [toDoItems, setToDoItems] = useState(todosData);
+
+  const handleChange = (id: number) => {
+    setToDoItems(toDoItems.map(i => {
+      if (i.id === id) {
+        i.completed = !i.completed;
+      }
+      return i;
+    }));
+  }
 
   return (
       <div className="todo-list">
-        {todosData.map(todoItem => <TodoItem key={todoItem.id} text={todoItem.text}
-                                             completed={todoItem.completed}/>)}
+        {toDoItems.map(todoItem =>
+            <TodoItem
+                key={todoItem.id}
+                id={todoItem.id}
+                text={todoItem.text}
+                completed={todoItem.completed}
+                handleChange={handleChange}
+            />)
+        }
       </div>
   );
 }
--- src/Components/TodoItem.tsx
+++ src/Components/TodoItem.tsx
@@ -1,15 +1,17 @@
 import React, {FC} from "react";
 
 interface ToDoItemProps {
+  id: number;
   text: string;
   completed: boolean;
+  handleChange: (id: number) => void;
 }
 
-const TodoItem: FC<ToDoItemProps> = ({text, completed}) => {
+const TodoItem: FC<ToDoItemProps> = ({id, text, completed, handleChange}) => {
   return (
       <div className="todo-item">
-        <input type="checkbox" checked={completed}/>
-        <p>{text}</p>
+        <input type="checkbox" checked={completed} onChange={() => handleChange(id)}/>
+        <p style={completed ? {color: '#cdcdcd', textDecoration: 'line-through'} : {}}>{text}</p>
       </div>
   );
 }
